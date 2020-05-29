13f874f46009de5538ac6ac924721fff93bb8784
jwy4877 <jwy4877@g.rit.edu>
2020-05-28 19:08:39 -0700

    + referenced package with a name + received information from API + checked response code for confirmation

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart';
+import 'package:http/http.dart' as http;
 
 import '../services/location.dart';
 
@@ -17,9 +17,15 @@ class _LoadingScreenState extends State<LoadingScreen> {
   }
 
   void getData() async {
-    Response response = await get(
+    http.Response response = await http.get(
         'https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=b6907d289e10d714a6e88b30761fae22');
-    print(response.body);
+
+    if (response.statusCode == 2000) {
+      String data = response.body;
+      print(data);
+    } else {
+      print(response.statusCode);
+    }
   }
 
   @override
